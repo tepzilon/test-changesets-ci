cfe9e6976b311245ba55d73591e3f9bd8fdbfe52
tepzilon <techid2000@gmail.com>
2025-04-01 22:03:01 +0700

    more

---
+++ scripts/ensureProductionPullRequest.ts
@@ -0,0 +1,29 @@
+import { Octokit } from '@octokit/rest'
+
+const OWNER = 'AlphaFinanceLab'
+const REPO = 'infinit-agents-monorepo'
+const HEAD = 'main'
+const BASE = 'production'
+
+const ensureProductionPullRequest = async () => {
+  if (process.env.GITHUB_TOKEN === undefined) {
+    console.error('GITHUB_TOKEN is not defined')
+    return
+  }
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  })
+  await octokit.rest.pulls.create({
+    owner: OWNER,
+    repo: REPO,
+    title: 'Merge main into production',
+    head: HEAD,
+    base: BASE,
+  })
+
+  // TODO: add pull request body with the updated CHANGELOG.md from the main branch to the production branch for every package
+}
+
+if (require.main === module) {
+  ensureProductionPullRequest()
+}
